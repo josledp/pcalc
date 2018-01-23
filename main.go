b7cefe9e62fb4f7fa12f23bcdd943365b1c4710f
José Luis Ledesma <joseluis.ledesma@socialpoint.es>
2018-01-23 09:03:05 +0100

    split parse

--- main.go
+++ main.go
@@ -38,9 +38,11 @@ func (p *pile) Pop() (*big.Float, error) {
 
 var p *pile
 
+var precission uint
+var pprecission int
+
 func main() {
-	var precission uint
-	var pprecission int
+
 	flag.UintVar(&precission, "precission", 512, "Setup float precission")
 	flag.IntVar(&pprecission, "print-precission", 64, "Setup float precission for printing")
 
@@ -56,67 +58,74 @@ MainFor:
 			if e == "" {
 				continue
 			}
-			match, err := regexp.MatchString("^[0-9\\.,]*$", e)
+			str, err := parse(e)
 			if err != nil {
-				log.Printf("error matching: %v", err)
+				log.Printf("error parsing %s: %v", e, err)
 				continue MainFor
 			}
-			if match {
-				f, _, err := big.ParseFloat(e, 10, precission, big.ToNearestEven)
-				if err != nil {
-					log.Printf("error parsing bigfloat: %v", err)
-					continue MainFor
-				}
-				p.Push(f)
-				continue
-			}
-			switch e {
-			case "p":
-				f, err := p.Pop()
-				if err != nil {
-					log.Println(err)
-					continue MainFor
-				}
-				fmt.Println(f.Text('f', pprecission))
-				p.Push(f)
-			case "n":
-				f, err := p.Pop()
-				if err != nil {
-					log.Println(err)
-					continue MainFor
-				}
-				fmt.Println(f.Text('f', pprecission))
-			case "pp":
-				f, err := p.Pop()
-				if err != nil {
-					log.Println(err)
-					continue MainFor
-				}
-				tmp, _ := f.Int64()
-				pprecission = int(tmp)
-			case "+":
-				err = dualOp(new(big.Float).Add)
-			case "*":
-				err = dualOp(new(big.Float).Mul)
-			case "/":
-				err = dualOp(new(big.Float).Quo)
-			case "-":
-				err = dualOp(new(big.Float).Sub)
-			case "^":
-				err = dualOp(bigfloat.Pow)
-			case "v":
-				err = singleOp(bigfloat.Sqrt)
-			case "q":
-				os.Exit(0)
-			default:
-				log.Printf("unkown command %s", e)
-			}
-			if err != nil {
-				log.Println(err)
-				continue MainFor
+			if str != "" {
+				fmt.Println(str)
 			}
 		}
 	}
+}
+
+func parse(e string) (string, error) {
+	if isFloat(e) {
+		f, _, err := big.ParseFloat(e, 10, precission, big.ToNearestEven)
+		if err != nil {
+			return "", fmt.Errorf("error parsing bigfloat: %v", err)
+		}
+		return "", p.Push(f)
+	}
+	switch e {
+	case "p":
+		f, err := p.Pop()
+		if err != nil {
+			return "", err
+		}
+		return f.Text('f', pprecission), p.Push(f)
+	case "n":
+		f, err := p.Pop()
+		if err != nil {
+			return "", err
+		}
+		return f.Text('f', pprecission), nil
+	case "pp":
+		f, err := p.Pop()
+		if err != nil {
+			return "", err
+		}
+		tmp, _ := f.Int64()
+		pprecission = int(tmp)
+	case "+":
+		return "", dualOp(new(big.Float).Add)
+	case "*":
+		return "", dualOp(new(big.Float).Mul)
+	case "/":
+		return "", dualOp(new(big.Float).Quo)
+	case "-":
+		return "", dualOp(new(big.Float).Sub)
+	case "^":
+		return "", dualOp(bigfloat.Pow)
+	case "v":
+		return "", singleOp(bigfloat.Sqrt)
+	case "q":
+		//FIXME: we should not do an os.Exit from here
+		os.Exit(0)
+	default:
+		return "", fmt.Errorf("unkown command")
+	}
+	return "", nil
+}
+
+func isFloat(s string) bool {
+	//this should never return an error
+	match, err := regexp.MatchString("^[0-9\\.,]*$", s)
+	if err != nil {
+		log.Printf("error matching: %v", err)
+	}
+	return match
 }
 
 func singleOp(operation func(f *big.Float) *big.Float) error {
@@ -124,11 +133,7 @@ func singleOp(operation func(f *big.Float) *big.Float) error {
 	if err != nil {
 		return err
 	}
-	err = p.Push(operation(f))
-	if err != nil {
-		return err
-	}
-	return nil
+	return p.Push(operation(f))
 }
 func dualOp(operation func(f1, f2 *big.Float) *big.Float) error {
 	f1, err := p.Pop()
@@ -139,9 +144,5 @@ func dualOp(operation func(f1, f2 *big.Float) *big.Float) error {
 	if err != nil {
 		return err
 	}
-	err = p.Push(operation(f2, f1))
-	if err != nil {
-		return err
-	}
-	return nil
+	return p.Push(operation(f2, f1))
 }
